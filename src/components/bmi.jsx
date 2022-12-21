1e36ad3d5d37f86e926d70420e21374b738a6181
Anubhuti Pandey <anubhutipandey028@gmail.com>
2022-12-21 14:06:21 +0530

    updated bmi

--- src/components/bmi.jsx
+++ src/components/bmi.jsx
@@ -1,71 +1,6 @@
-// import React from "react";
-// import { useState, useEffect} from "react";
-
-//     function Box (){
-//     const [inputText, setInputText] = useState("");
-//     const [inpText, setInpText] = useState("");
-//     const handleSubmit = (event) => {
-//       event.preventDefault();
-//     // {inputText!=="" && inpText!=="" &&
-//     // <Bmi inputText={inputText} inpText={inpText}/>}
-//   }
-//   return (
-//     <form>
-//       <label>Enter your weight :
-//         <input className="bg-lime-200 mt-20"
-//           type="number"
-//           value={inputText}
-//           onChange={(e) => setInputText(e.target.value)}
-//           />
-//       </label>
-//       <label>Enter your height :
-//         <input className="bg-lime-200 mt-10"
-//           type="number"
-//           value={inpText}
-//           onChange={(e) => setInpText(e.target.value)}
-//           />
-//         <br/>
-//         </label>
-//       <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-10" onSubmit={handleSubmit}>submit</button>
-//       {inputText!=="" &&
-//       <Bmi inputText={inputText} inpText={inpText}/>}
-//     </form>
-//     )};
-//     export default function Bmi({inputText},{inpText}) {
-
-//       const [data, setData] = useState(null);
-//       const [loading, setLoading] = useState(true);
-//       const options = {
-//         method: 'GET',
-//         headers: {
-//           'X-RapidAPI-Key': '212a5a0b60mshf0bfcae157f86edp1bb264jsn630a6d90f2fc',
-//           'X-RapidAPI-Host': 'mega-fitness-calculator1.p.rapidapi.com'
-//         }
-//         };
-//       useEffect(() => {
-//         fetch(`https://mega-fitness-calculator1.p.rapidapi.com/bmi?weight=${inputText}&height=${inpText}`, options)
-//         .then(response => response.json())
-//         .then((usefulData) => {
-//           const infoText = usefulData.info.bmi;
-//           setData(infoText);
-//           console.log(usefulData);
-//           setLoading(false);
-//         })
-//       }, [inputText,inpText]);
-//       return (
-//         <>
-//           <div className="App text-6xl text-center font-serif">
-//             {loading && <p>hi</p>}
-//             <Box/>
-//             {!loading &&<p id='infoElement'>{data}</p>
-//             }
-//           </div>
-//         </>
-//       )
-//     }
 import React, { useState } from "react";
 
-const App = () => {
+const Bmi = () => {
   const [bmii, setBmii] = useState("");
   const [weight, setWeight] = useState("");
   const [height, setHeight] = useState("");
@@ -106,32 +41,37 @@ const App = () => {
 
   return (
     <>
-      <form className="App text-6xl text-center font-serif">
+      <form className="text-4xl md:text-6xl text-center font-serif ">
+        <div className="flex flex-col">
         <label>
           Enter your weight :
-          <input className="bg-lime-200 mt-20"
+          <input className="bg-lime-200 lg:mt-20 my-5"
             type="number"
             value={weight}
             onChange={handleweight}
+            placeholder="weight in kg"
             required
           />
         </label>
         <label>
-          Enter your height :
-          <input className="bg-lime-200 mt-20"
+         Enter your height:
+          <input className="bg-lime-200 lg:mt-20 my-5 "
             type="number"
             value={height}
             onChange={handleHeight}
+            placeholder="height in cm"
             required
           />
         </label>
+        </div>
         <button type="submit" onClick={handleSubmit} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-10" >
-          sumbit
+          :click:
         </button>
+        <p>Your BMI is:</p>
         {bmii ? bmii.info.bmi : []}
       </form>
     </>
   );
 };
 
-export default App;
+export default Bmi;
