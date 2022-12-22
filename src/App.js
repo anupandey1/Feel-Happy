38469dc2f9504607b51d3ae221865fb7940662f1
Anubhuti Pandey <anubhutipandey028@gmail.com>
2022-12-22 19:57:02 +0530

    made fixed navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
 import './App.css';
-import {Routes, Route, Link } from "react-router-dom";
-import Landing from "./components/landing";
+import { Routes, Route, Link } from 'react-router-dom';
+import logo from './assets/WebTitle.svg';
+import Landing from './components/landing';
 import Jokes from './components/jokes';
 import Bmi from './components/bmi';
-import Atmos from './components/atmos';
+
 function App() {
   return (
-    <div className="App">
-      <nav>
-      <ul>
-       <li><Link to="/">Home</Link></li>
-       <li> <Link to="/bmi">BMI</Link></li>
-       <li> <a href="https://anupandey1.github.io/Atmos/">ATMOS</a></li>
-       <li> <Link to="/jokes">GIGGLES</Link></li>
-       </ul>
-       </nav>
+    <div className="nav">
+      <nav >
+        <ul>
+          <div className="flex justify-between items-center ">
+            <li>
+              <img src={logo} className="w-44 h-16 ml-5" />
+            </li>
+            <div className="flex flex-row text-xl font-sans font-semibold">
+              <li className="mx-5 my-3 hover:scale-125 hover:underline">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="mx-5 my-3 hover:scale-125 hover:underline ">
+                <Link to="/bmi">Bmi</Link>
+              </li>
+              <li className="mx-5 my-3 hover:scale-125 hover:underline">
+                <a href="https://anupandey1.github.io/Atmos/">Atmos</a>
+              </li>
+              <li className="mx-5 my-3 hover:scale-125 hover:underline">
+                <Link to="/jokes">Giggles</Link>
+              </li>
+            </div>
+          </div>
+        </ul>
+      </nav>
       <Routes>
-          <Route path="/" element={<Landing/>}/>
-          <Route path="bmi" element={<Bmi/>} />
-          <Route path="jokes" element={<Jokes />} />
-       </Routes> 
+        <Route path="/" element={<Landing />} />
+        <Route path="bmi" element={<Bmi />} />
+        <Route path="jokes" element={<Jokes />} />
+      </Routes>
     </div>
   );
 }
